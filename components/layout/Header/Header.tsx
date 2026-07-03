68d80ac258e1334718e5c8183c84501ef50392c4
Stepan <stepanhv@yandex.ru>
2026-07-03 13:50:45 +0700

    Install react select

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,3 +1,4 @@
+// 'use client';
 import styles from './Header.module.scss';
 import LogoImage from '@/public/icons/logo.svg';
 import Image from 'next/image';
@@ -6,6 +7,34 @@ import { Button } from '@/components/ui/Button/Button';
 import { Burger } from '@/components/ui/Burger/Burger';
 import { OpenRequest } from './OpenRequestForm';
 import { MASKED_REQUEST_PHONE, REQUEST_PHONE } from '@/constants';
+
+// import Select, { components, ControlProps } from 'react-select';
+
+// type Option = {
+//   label: string;
+//   value: string;
+// };
+
+// const CustomControl = ({ children, ...props }: ControlProps<Option>) => {
+//   // return (
+//   //   <div style={{ border: '2px solid purple', borderRadius: '8px' }}>
+//   //     <span>🌟</span>
+//   //     {/* 2. Wrap and spread props to retain default logic */}
+//   //     <components.Control {...props}>{children}</components.Control>
+//   //   </div>
+//   // );
+//   return (
+//     <div>
+//       <Button
+//         variant="primary"
+//         label="Выбрать квартиру"
+//         size="small"
+//         className={styles.select}
+//       />
+//       <components.Control {...props}>{children}</components.Control>
+//     </div>
+//   );
+// };
 
 export const Header = () => {
   return (
@@ -20,14 +49,48 @@ export const Header = () => {
         size="small"
         className={styles.select}
       />
+      {/* <Select
+        components={{
+          Control: CustomControl,
+          // IndicatorSeparator: () => null,
+          // DropdownIndicator: () => null,
+          // ClearIndicator: () => null,
+          Input: () => null,
+          Placeholder: () => null,
+          ValueContainer: (props) => null,
+          Option: (props) => {
+            const { value, label } = props.data;
+
+            return (
+              <components.Option {...props}>
+                <Link href={'/test'}>{label}</Link>
+              </components.Option>
+            );
+          },
+        }}
+        options={[
+          { value: 'chocolate', label: 'Chocolate' },
+          { value: 'strawberry', label: 'Strawberry' },
+          { value: 'vanilla', label: 'Vanilla' },
+        ]}
+        isSearchable={false}
+
+        controlShouldRenderValue={false}
+
+        placeholder=""
+
+        blurInputOnSelect={false}
+
+        backspaceRemovesValue={false}
+
+        tabSelectsValue={false}
+      /> */}
       <Link href="/" className={styles.logo}>
         <Image src={LogoImage} alt={'Логотип Inchapin'} />
       </Link>
-
       <Link href={`tel:${REQUEST_PHONE}`} className={styles.phone}>
         {MASKED_REQUEST_PHONE}
       </Link>
-
       <OpenRequest className={styles.order} />
     </header>
   );
